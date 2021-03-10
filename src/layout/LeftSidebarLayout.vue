6279ecf056ef1bc25958e5c55b14f08504e864a3
Dadson Papa Dadze <pddadson@gmail.com>
2021-03-10 15:53:31 +0000

    entire bar clickable

--- src/layout/LeftSidebarLayout.vue
+++ src/layout/LeftSidebarLayout.vue
@@ -1,34 +1,39 @@
 <template>
   <div class="flex flex-col md:flex-row h-full overflow-hidden">
     <div
-      class="border-r overflow-hidden transition-all duration-500 ease-in-out relative"
+      class="border-r overflow-hidden relative transition-all duration-500 ease-in-out"
       :class="{ 'w-12': !isOpen, 'w-1/5': isOpen }"
     >
-      <div
-        v-if="!isOpen"
-        class="absolute flex items-center justify-center top-0 left-0 w-12 h-12 bg-warning transition-all duration-500 ease-in-out"
-        @click="handleOpen"
-      >
-        <img src="@/assets/img/user--activity 1.svg" class="w-5 h-5" alt="" />
-      </div>
       <div>
-        <slot v-if="isOpen" name="sidebar" />
-        <div class="absolute bottom-0 left-0 h-12 bg-black flex w-full">
-          <div
-            class="w-full pr-4 flex transition-all duration-500 ease-in-out justify-end items-center bg-serenity-light-gray"
-            :class="{ hidden: !isOpen, block: isOpen }"
-          >
-            <p class="text-xs">Minimize panel</p>
+        <div
+          v-if="!isOpen"
+          class="absolute flex items-center justify-center top-0 left-0 w-12 h-12 bg-warning transition-all duration-500 ease-in-out"
+          @click="isOpen = !isOpen"
+        >
+          <img src="@/assets/img/user--activity 1.svg" class="w-5 h-5" alt="" />
+        </div>
+        <div>
+          <div>
+            <slot v-if="isOpen" name="sidebar" />
           </div>
-          <div
-            class="w-12 flex items-center justify-center h-12 bg-black"
-            @click="handleOpen"
-          >
-            <ArrowRight
-              class="w-5 h-5 text-white transition-all duration-50 ease-in-out transform"
-              :class="[isOpen ? 'rotate-180' : 'rotate-0']"
-            />
-          </div>
+        </div>
+      </div>
+      <div class="h-full" @click="conditionalOpening"></div>
+      <div class="absolute bottom-0 left-0 h-12 bg-black flex w-full">
+        <div
+          class="w-full pr-4 flex transition-all duration-500 ease-in-out justify-end items-center bg-serenity-light-gray"
+          :class="{ hidden: !isOpen, block: isOpen }"
+        >
+          <p class="text-xs">Minimize panel</p>
+        </div>
+        <div
+          class="w-12 flex items-center justify-center h-12 bg-black"
+          @click="isOpen = !isOpen"
+        >
+          <ArrowRight
+            class="w-5 h-5 text-white transition-all duration-50 ease-in-out transform"
+            :class="[isOpen ? 'rotate-180' : 'rotate-0']"
+          />
         </div>
       </div>
     </div>
@@ -55,15 +60,12 @@ export default {
   },
 
   methods: {
-    // conditionalOpening() {
-    //   if (this.isOpen) {
-    //     return;
-    //   }
+    conditionalOpening() {
+      if (this.isOpen) {
+        return;
+      }
 
-    //   this.isOpen = true;
-    // },
-    handleOpen() {
-      this.isOpen = !this.isOpen;
+      this.isOpen = true;
     },
   },
 };
