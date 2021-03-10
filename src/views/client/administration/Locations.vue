eaabc41bb242af7b0d0b923f0c68ebe7247f44b0
Dadson Papa Dadze <pddadson@gmail.com>
2021-03-10 02:34:58 +0000

    locations table & search

--- src/views/client/administration/Locations.vue
+++ src/views/client/administration/Locations.vue
@@ -8,21 +8,14 @@
         kind="primary"
         @click="$trigger('location:add:open')"
       >
-        Add new location 
+        Add new location
         <Add class="ml-4 w-5 h-5 text-white" />
       </cv-button>
     </div>
 
-    <cv-search
-      v-model="search"
-      placeholder="Search for location"
-    />
-
-    <cv-data-table
-      ref="table"
-      :data="filteredLocations"
-      :columns="columns"
-    >
+    <cv-search v-model="search" placeholder="Search for location" />
+    <!-- {{ filteredLocations }} -->
+    <cv-data-table ref="table" :data="filteredLocations" :columns="columns">
       <template slot="data">
         <cv-data-table-row
           v-for="(row, rowIndex) in filteredLocations"
@@ -34,30 +27,30 @@
               <p>{{ row.location_name }}</p>
             </div>
           </cv-data-table-cell>
-            
-          <!-- <cv-data-table-cell>
+
+          <cv-data-table-cell>
+            <div class="flex items-center space-x-2 py-2">
+              <p>{{ row.street_address }}</p>
+            </div>
+          </cv-data-table-cell>
+
+          <cv-data-table-cell>
             <div class="flex items-center space-x-6">
               <p
                 class="cursor-pointer"
-                @click="$trigger('location:edit:open', {...row})"
+                @click="$trigger('location:edit:open', { ...row })"
               >
-                Edit
+                View
               </p>
-              <p
-                class="text-red-500 cursor-pointer"
-                @click="remove(row.id)"
-              >
+              <p class="text-red-500 cursor-pointer" @click="remove(row.id)">
                 Delete
               </p>
             </div>
-          </cv-data-table-cell> -->
+          </cv-data-table-cell>
         </cv-data-table-row>
       </template>
     </cv-data-table>
-    <p
-      v-if="!locations.length"
-      class="text-center w-full py-6"
-    >
+    <p v-if="!locations.length" class="text-center w-full py-6">
       No locations to show
     </p>
     <AddEditLocation />
@@ -65,20 +58,16 @@
 </template>
 
 <script>
-import AddEditLocation from '@/components/admin/modals/AddEditLocation'
-import { mapActions, mapState } from 'vuex'
+import AddEditLocation from "@/components/admin/modals/AddEditLocation";
+import { mapActions, mapState } from "vuex";
 export default {
-  components: {AddEditLocation},
+  components: { AddEditLocation },
   data() {
     return {
-      search: '',
-      columns: [
-        'Name',
-        'Address',
-        'City',
-      ],
+      search: "",
+      columns: ["Name", "Address", "Action"],
       loading: false,
-    }
+    };
   },
 
   computed: {
@@ -86,31 +75,39 @@ export default {
       locations: (state) => state.locations.locations,
     }),
     filteredLocations() {
-      return this.locations.filter(data => !this.search || data.name.toLowerCase().includes(this.search.toLowerCase()))
+      return this.locations.filter(
+        (data) =>
+          !this.search ||
+          data.location_name
+            .toLowerCase()
+            .includes(this.search.toLowerCase()) ||
+          data.city.toLowerCase().includes(this.search.toLowerCase()) ||
+          data.street_address.toLowerCase().includes(this.search.toLowerCase())
+      );
     },
   },
 
   created() {
-    this.refresh()
+    this.refresh();
   },
 
   methods: {
     ...mapActions({
-      getLocations: 'locations/getLocations',
-      deleteLocation: 'locations/deleteLocation',
+      getLocations: "locations/getLocations",
+      deleteLocation: "locations/deleteLocation",
     }),
 
     async refresh() {
-      this.loading = true
-      await this.getLocations()
-      this.loading = false
+      this.loading = true;
+      await this.getLocations();
+      this.loading = false;
     },
 
     async remove(id) {
-      this.loading = true
-      await this.deleteLocation(id)
-      this.loading = false
+      this.loading = true;
+      await this.deleteLocation(id);
+      this.loading = false;
     },
   },
-}
+};
 </script>
