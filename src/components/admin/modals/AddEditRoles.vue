23b37cde59940439702e472a5474eb72f2c9e0ce
Dadson Papa Dadze <pddadson@gmail.com>
2021-03-24 11:47:48 +0000

    empty string

--- src/components/admin/modals/AddEditRoles.vue
+++ src/components/admin/modals/AddEditRoles.vue
@@ -107,7 +107,7 @@ export default {
   data() {
     return {
       form: {
-        name: 'Account',
+        name: '',
         permissions: {
           workspaces: [],
           resources: [],
