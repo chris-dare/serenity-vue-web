da0b9d0ea7bdd29cf29e37553425e5153fc3389b
Caleb Kpabitey <calebkpabitey@gmail.com>
2021-05-16 15:39:46 +0000

    Pharmacy screens cherrypick changes

---
+++ src/views/client/pharmacy/Pharmacy.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="w-4/5 mx-auto">
+    <UserDetailsHeader />
+    <p class="text-serenity-primary my-6 font-semibold">
+      What would you like to do?
+    </p>
+    <div class="flex my-4">
+      <PatientCard
+        v-for="(dashboard, index) in availableActions"
+        :key="index"
+        :is-selected="selected === dashboard.value"
+        :details="dashboard"
+        :type="dashboard.type"
+        custom-class="bg-white border-0"
+        @click="change(dashboard)"
+      />
+    </div>
+    <GetStartedModal :visible.sync="visible" />
+    <NonPatientPrescriptionModal />
+  </div>
+</template>
+
+<script>
+import PatientCard from '@/components/appointments/PatientCard'
+import GetStartedModal from '@/components/dashboard/GetStartedModal'
+import NonPatientPrescriptionModal from '@/components/pharmacy/modals/NonPatientPrescriptionModal'
+import UserDetailsHeader from '@/components/ui/headers/UserDetailsHeader'
+import { mapState } from 'vuex'
+
+export default {
+  name: 'Dashboard',
+
+  components: { 
+    PatientCard,
+    GetStartedModal,
+    NonPatientPrescriptionModal,
+    UserDetailsHeader,
+  },
+
+  data() {
+    return {
+      selected: 'search',
+      visible: false,
+      visitVisible: false,
+    }
+  },
+
+  computed: {
+    ...mapState({
+      workspaceType: (state) => state.global.workspaceType,
+      appointmentsCount: (state) => state.appointments.appointmentsCount,
+    }),
+
+    availableActions() {
+      const types = [
+        {
+          label: 'Find a patient',
+          description: 'Quickly search for a patient by name',
+          type: 'search',
+          value: 'search',
+        },
+        {
+          label: 'Non Patient',
+          description: 'Dispense drugs to non-hospital patients',
+          type: 'search',
+          value: 'nonpatient',
+        },
+      ]
+
+      return types
+    },
+  },
+
+  methods: {
+    change(dashboard) {
+      this.selected = dashboard.value
+
+      if (dashboard.value === 'search') {
+        this.$router.push({ name: 'Pharmacy:Patients'})
+      }
+
+      if (dashboard.value === 'nonpatient') {
+        this.$trigger('pharmacy:nonpatient_prescription:open')
+      }
+      
+    },
+  },
+}
+</script>
