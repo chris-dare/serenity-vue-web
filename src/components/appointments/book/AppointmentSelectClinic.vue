f966ca22fbf9402cf4dca42eaedfa2939a3ff8c1
seddie62 <seddie62@gmail.com>
2021-06-03 01:44:41 +0000

    label changed

--- src/components/appointments/book/AppointmentSelectClinic.vue
+++ src/components/appointments/book/AppointmentSelectClinic.vue
@@ -9,7 +9,7 @@
     @save="validateAndReroute"
   >
     <p class="text-primary my-4">
-      Why are you booking this appointment?
+      {{ modal ? 'Reason for visit' : 'Why are you booking this appointment?' }}
     </p>
     <div class="grid grid-cols-3 gap-4">
       <InfoLinkCard
