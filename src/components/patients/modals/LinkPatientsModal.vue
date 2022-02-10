41ea9244da85722661733a37765b8a77ec227828
seddie62 <seddie62@gmail.com>
2022-02-10 11:35:22 +0000

    link patients

---
+++ src/components/patients/modals/LinkPatientsModal.vue
@@ -0,0 +1,143 @@
+<template>
+  <BaseModal
+    :name="name"
+    :title="title"
+    width="450px"
+    @closed="close"
+  >
+    <template>
+      <div class="space-y-4">
+        <div
+          v-if="step === 1"
+          class="space-y-6"
+        >
+          <p>Select the main account you want to link all other patients to</p>
+          <div class="space-y-2">
+            <div class="space-y-4">
+              <div
+                v-for="(patient, index) in patients"
+                :key="index"
+                class="flex items-center justify-between bg-serenity-light-gray px-4 py-2"
+              >
+                <div class="flex space-x-2 items-center">
+                  <ImageBlock
+                    :url="patient.photo"
+                    :alt="(patient.name || patient.fullName) | capitalize"
+                    custom-class="w-12 h-12"
+                  />
+                  <div>
+                    <p class="text-sm font-semibold">{{ patient.mr_number }}</p>
+                    <p class="text-sm">{{ (patient.name || patient.fullName) | capitalize }}</p>
+                    <p class="text-secondary text-sm capitalize">
+                      {{ patient.gender_age_description }}
+                    </p>
+                  </div>
+                </div>
+
+                <cv-radio-button
+                  v-model="form.patient"
+                  :value="patient.id"
+                />
+              </div>
+            </div>
+          </div>
+          <SeButton
+            full
+            :disabled="$v.$invalid"
+            @click="step = 2"
+          >
+            Next
+          </SeButton>
+        </div>
+        <div
+          v-else-if="step === 2"
+          class="space-y-6"
+        >
+          <p>Give insights to why you are linking the patient data.</p>
+          <FormInput
+            v-model="form.notes"
+            type="textarea"
+            :rows="8"
+          />
+          <div class="flex items-center justify-between">
+            <SeButton
+              variant="secondary"
+              @click="step = 1"
+            >
+              Back
+            </SeButton>
+            <SeButton
+              :disabled="$v.$invalid"
+              @click="submit"
+            >
+              Next
+            </SeButton>
+          </div>
+        </div>
+      </div>
+    </template>
+  </BaseModal>
+</template>
+
+<script>
+import modalMixin from '@/mixins/modal'
+import { required, requiredIf } from 'vuelidate/lib/validators'
+
+export default {
+  name: 'LinkPatientsModal',
+
+
+  mixins: [modalMixin],
+
+  data() {
+    return {
+      form: {},
+      name: 'link-patients-modal',
+      patients: [],
+      step: 1,
+      selected: null,
+    }
+  },
+
+  validations() {
+    return {
+      form: {
+        patient: { required },
+        notes: {
+          required: requiredIf(() => {
+            return this.step === 2
+          }),
+        },
+      },
+    }
+  },
+
+  events: {
+    'patients:link:open': function({params}){
+      this.open()
+      this.patients = params.map(patient => {
+        patient.name = this.$utils.concatData(patient, ['first_name', 'last_name'])
+        patient.gender_age_description = patient.age ? `${this.$utils.concatData(patient, ['gender', 'age'], ', ')} years` : this.$utils.concatData(patient, ['gender', 'age'], ', ')
+        return patient
+      })
+    },
+    'patients:link:close': function(){
+      this.close()
+    },
+  },
+
+  computed: {
+    title() {
+      let titles = {
+        1: 'Choose Primary Patient',
+        2: 'Link Patient Data',
+        3: '',
+      }
+
+      return titles[this.step]
+    },
+  },
+
+  methods: {},
+}
+</script>
